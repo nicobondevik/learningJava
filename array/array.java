267dc65bceb8cade42db6590f63429d26c216bf9
Jon Nicolas <jonnico96@icloud.com>
2022-03-22 16:03:26 +0100

    repo cleanup

--- array/array.java
+++
@@ -1,9 +0,0 @@
-public class array{
-	public static void main(String[] args){
-		int[] arr = {1,2,3,4,5,6,7,8,9,10};
-		System.out.printf("%s%8s%n", "Index", "Value");
-		for (int i = 0; i < arr.length; i++){
-			System.out.printf("%5d%8d%n", i, arr[i]);
-		}
-	}
-}
